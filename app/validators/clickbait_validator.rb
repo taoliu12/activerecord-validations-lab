91725cc7ff971d7a9f7b13f3ff3a145f143cce19
taoliu12 <taoliudesign@gmail.com>
2020-09-18 23:48:50 -0700

    Done.

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -3,9 +3,10 @@ class ClickbaitValidator < ActiveModel::Validator
     # If the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess", the validator should add a validation.
 
     def validate(record)
-        non_clickbait = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+        non_clickbait_titles = ["Won't Believe", "Secret", "Top[number]", "Guess"]
 
-        if record.title && !non_clickbait.detect {|e| record.title.include?(e)}
+        if record.title && non_clickbait_titles.none? {|e| record.title.include?(e)}
+        # if record.title && non_clickbait_titles.detect {|e| record.title.include?(e)}.nil?        
             record.errors[:title] << "Must contain clickbait"           
         end
       end
